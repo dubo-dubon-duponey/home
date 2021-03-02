5b9290c405545e928f28251b8b3a387d4c4c4884
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-03-02 15:38:31 -0800

    Reg
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- modules/registry/custom.tf
+++ modules/registry/custom.tf
@@ -1,30 +1,34 @@
 # Local indirection
 locals {
-  capabilities  = var.user == "root" ? ["NET_BIND_SERVICE"] : []
-  command       = []
-  devices       = []
-  env           = [
-    "DOMAIN=${var.domain}",
-    "PULL=${var.pull}",
-    "PUSH=${var.push}",
-    "PORT=${local.internal_port}",
+  // in-container port for the service - this will be public facing in case of a vlan or host network
+  service_port      = (var.user == "root" ? var.port : local.defaults.port)
+  // if at least one of the networks is a bridge, and if expose is true
+  container_expose  = var.expose ? {
+    (var.port): local.service_port,
+  } : {}
+
+  service_domain    = (var.domain != "" ? var.domain : "${local.container_name}.local")
+  mdns_host         = (var.mdns_host != "" ? var.mdns_host : local.container_name)
+  mdns_name         = (var.mdns_name != "" ? var.mdns_name : local.mdns_host)
+
+  env = [
+    "LOG_LEVEL=warn",
+    "TLS=${var.tls}",
+    "DOMAIN=${local.service_domain}",
+    "PORT=${local.service_port}",
     "USERNAME=${var.username}",
     "PASSWORD=${var.password}",
-    // TODO make these configurable
-    "MDNS=enabled",
-    "TLS=internal",
-    "SALT=eW91IGFyZSBzbyBzbWFydAo=",
-    // XXX remove this with the new version of the registry
-    "REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/data",
-    "USERNAME_ADMIN=${var.username}",
-    "PASSWORD_ADMIN=${var.password}",
+    "REALM=${var.realm}",
+    "MDNS_ENABLED=${var.mdns_enabled}",
+    "MDNS_HOST=${local.mdns_host}",
+    "MDNS_NAME=${local.mdns_name}",
+
+    "PULL=${var.pull}",
+    "PUSH=${var.push}",
   ]
-  expose        = var.expose ? {
-    (var.port): local.internal_port,
-  } : {}
-  expose_type   = "tcp"
-  group_add     = []
-  labels        = {}
+}
+
+locals {
   mounts        = {}
   mountsrw      = {
     "/data": var.data_path,
@@ -33,8 +37,6 @@ locals {
   volumes       = {
     "/tmp": docker_volume.tmp.name
   }
-
-  internal_port = (var.user == "root" ? var.port : 4443)
 }
 
 resource "docker_volume" "tmp" {
@@ -42,44 +44,27 @@ resource "docker_volume" "tmp" {
   name          = "tmp-${local.container_name}"
 }
 
-# Service settings
-variable "expose" {
-  description = "Whether to expose ports (only applicable to bridge networking)"
-  type        = bool
-  default     = false
-  // TODO enable after 0.14 sensitive   = false
+variable "data_path" {
+  description = "Host path for persistent data & config"
+  type        = string
+  default     = "/home/container/data/registry"
 }
 
-variable "username" {
-  description = "Restricted access to username"
+variable "cert_path" {
+  description = "Host path for persistent data & config"
   type        = string
-  // TODO enable after 0.14 sensitive   = true
-  // TODO enforce minimum length
-}
-
-variable "password" {
-  description = "Restricted access password"
-  type        = string
-  // TODO enable after 0.14 sensitive   = true
-  // TODO enforce length and content
-}
-
-variable "domain" {
-  description = "Domain name to advertise (must be in the form of FOO.local if mDNS is enabled)"
-  type        = string
-  default     = "registry.local"
-  // TODO enable after 0.14 sensitive   = false
-  // TODO valid domain name
+  // TODO move this away later on to a central (non service dependent location)
+  // and/or mount the root cert from a location
+  default     = "/home/container/certs/registry"
 }
 
 variable "pull" {
-  description = "Whether to enable push access (anonymous or authenticated), or disable it (disabled)"
+  description = "Whether to enable pull access (anonymous or authenticated), or disable it (disabled)"
   type        = string
   default     = "authenticated"
-  // TODO enable after 0.14 sensitive   = false
   validation {
     condition     = can(regex("^(?:disabled|anonymous|authenticated)$", var.pull))
-    error_message = "Pull must be disabled, anonymous or authenticated."
+    error_message = "Pull must be one of: 'disabled', 'anonymous' or 'authenticated'."
   }
 }
 
@@ -87,37 +72,8 @@ variable "push" {
   description = "Whether to enable push access (authenticated), or disable it (disabled)"
   type        = string
   default     = "disabled"
-  // TODO enable after 0.14 sensitive   = false
   validation {
     condition     = can(regex("^(?:disabled|authenticated)$", var.push))
-    error_message = "Push must be disabled or authenticated."
-  }
-}
-
-variable "data_path" {
-  description = "Host path for persistent data & config"
-  type        = string
-  default     = "/home/container/data/registry"
-  // TODO enable after 0.14 sensitive   = false
-  // TODO valid path
-}
-
-variable "cert_path" {
-  description = "Host path for persistent data & config"
-  type        = string
-  default     = "/home/container/certs/registry"
-  // TODO enable after 0.14 sensitive   = false
-  // TODO valid path
-}
-
-variable "port" {
-  description = "Main port to expose"
-  type        = number
-  default     = 443
-  // TODO enable after 0.14 sensitive   = false
-  validation {
-    // TODO figure out how to honor root vs. normal user
-    condition     = var.port > 0  && var.port < 65536 // && (var.user == "root" || var.port > 1023)
-    error_message = "The port must be in the range 1-65535."
+    error_message = "Push must be either 'disabled' or 'authenticated'."
   }
 }
