5ca03f5103f7addf3eb3d7b644a6b3c35de4a199
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2023-01-03 12:30:15 -0800

    Bump Terraform version
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- modules/audio-receiver/versions.tf
+++ modules/audio-receiver/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
-      version = ">= 2.15, < 2.16"
+      version = ">= 2.23, < 2.24"
     }
   }
-  required_version = ">= 1.0, < 1.2"
+  required_version = ">= 1.0, < 1.4"
 }
